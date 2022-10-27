98ca0ba73f4300d4ed02fa8a48f70fb5a55ba8b3
Azpers <Azpers@github.com>
2022-10-27 17:50:51 -0300

    info displaying on page as expected after being called by fetch and be brought with innerhtml

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,25 @@
 const main = document.querySelector('main');
+
+/* FETCH FUNCTIONS */
+
+fetch('https://randomuser.me/api/?inc=name,location,email,dob,cell,picture&results=12&noinfo')
+    .then(response => response.json())
+    .then(data => generateData(data))
+
+
+/* HELPER FUNCTIONS */
+
+function generateData (data) {
+    data.results.map(result => {
+        const employeeDiv = document.createElement('Div');        
+        main.appendChild(employeeDiv);
+            employeeDiv.innerHTML = `
+                <div class="card">
+                <img src="${result.picture.medium}">
+                <p class="bold | name-fs">${result.name.first} ${result.name.last}</p>
+                <p class="">${result.email}</p>
+                <p class="">${result.location.city}</p>
+                </div>`
+    })
+}
+
