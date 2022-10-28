412f625ab06f02ef83c6732c6afc90899c1bddf0
Azpers <Azpers@github.com>
2022-10-28 02:38:04 -0300

    modal starting to operate, gotta check on css so it looks as we want to

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
 const main = document.querySelector('main');
 
-/* FETCH FUNCTIONS */
 
-fetch('https://randomuser.me/api/?inc=name,location,email,dob,cell,picture&results=12&noinfo')
+/* FETCH FUNCTIONS */
+    fetch('https://randomuser.me/api/?inc=name,location,email,dob,cell,picture&results=12&noinfo')
     .then(response => response.json())
-    .then(data => generateData(data));
+    .then(data => generateData(data))
+    // .then(data => modalStepsIn(data))
+
 
 
 /* HELPER FUNCTIONS */
 
-function generateData (data) {
+function generateData(data) {
     data.results.map(result => {
         const employeeDiv = document.createElement('Div'); 
         employeeDiv.classList.add('card');       
@@ -20,20 +22,45 @@ function generateData (data) {
                 <p class="ppl-details-fs">${result.email}</p>
                 <p class="ppl-details-fs">${result.location.city}</p>
                 </div>`;
+                //NEW MODAL ATTEMPT V2
+                employeeDiv.addEventListener('click', () => {
+                    const modalWrapper = document.createElement('Div');
+                    modalWrapper.classList.add('modal');
+                    modalWrapper.innerHTML = `
+                    <div class="modal-content">
+                        <span class="close-button"></span>
+                        <img src="${result.picture.medium}" class="img-style">
+                        <p class="name-fs">${result.name.first} ${result.name.last}</p>
+                        <p class="ppl-details-fs">${result.email}</p>
+                        <p class="ppl-details-fs">${result.location.city}</p>
+                    </div>
+                    `;
+                    main.appendChild(modalWrapper);
+                }); //console.log(result.cell)
         main.appendChild(employeeDiv);
     });
+    // const allCards = main.getElementsByClassName('card');
+    return data
+}
+
+/* MODAL ATTEMPT V1*/
+
+function modalStepsIn(data) {
+    const modalWrapper = document.createElement('Div');
+    modalWrapper.classList.add('modal');
+    modalWrapper.innerHTML = `
+    <div class="modal-content">
+        <span class="close-button"></span>
+        <img src="${result.picture.medium}" class="img-style">
+        <p class="name-fs">${result.name.first} ${result.name.last}</p>
+        <p class="ppl-details-fs">${result.email}</p>
+        <p class="ppl-details-fs">${result.location.city}</p>
+    </div>
+    `;
+    main.appendChild(modalWrapper);
 }
 
 
 
 
-/* THIS CODE BELOW ELIMINATES ISSUES WITH SAMCOOKIES
-More info on the following link:
-https://support.cookiehub.com/article/84-cookie-security-and-the-samesite-attribute?gclid=CjwKCAjw2OiaBhBSEiwAh2ZSP4lOaJf4kMYk_TnG1dvw92MaCdBP5O9yM75MNM87QBa4H2iPm4pDbBoCfNsQAvD_BwE */
-// var cpm = {};
-// (function(h,u,b){
-// var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
-// e.async=true;e.src='https://cookiehub.net/c2/xxxxxxxx.js';
-// e.onload=function(){u.cookiehub.load(b);}
-// d.parentNode.insertBefore(e,d);
-// })(document,window,cpm);
+ 
