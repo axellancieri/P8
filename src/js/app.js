cce1398deb5657e60101c77f3d0e70889d7b7e19
Azpers <Azpers@github.com>
2022-10-27 21:27:28 -0300

    styled and looking good, now starting to research on how to display a modal

--- src/js/app.js
+++ src/js/app.js
@@ -4,22 +4,36 @@ const main = document.querySelector('main');
 
 fetch('https://randomuser.me/api/?inc=name,location,email,dob,cell,picture&results=12&noinfo')
     .then(response => response.json())
-    .then(data => generateData(data))
+    .then(data => generateData(data));
 
 
 /* HELPER FUNCTIONS */
 
 function generateData (data) {
     data.results.map(result => {
-        const employeeDiv = document.createElement('Div');        
-        main.appendChild(employeeDiv);
+        const employeeDiv = document.createElement('Div'); 
+        employeeDiv.classList.add('card');       
             employeeDiv.innerHTML = `
-                <div class="card">
-                <img src="${result.picture.medium}">
-                <p class="bold | name-fs">${result.name.first} ${result.name.last}</p>
-                <p class="">${result.email}</p>
-                <p class="">${result.location.city}</p>
-                </div>`
-    })
+                <img src="${result.picture.medium}" class="img-style">
+                <div>
+                <p class="name-fs">${result.name.first} ${result.name.last}</p>
+                <p class="ppl-details-fs">${result.email}</p>
+                <p class="ppl-details-fs">${result.location.city}</p>
+                </div>`;
+        main.appendChild(employeeDiv);
+    });
 }
 
+
+
+
+/* THIS CODE BELOW ELIMINATES ISSUES WITH SAMCOOKIES
+More info on the following link:
+https://support.cookiehub.com/article/84-cookie-security-and-the-samesite-attribute?gclid=CjwKCAjw2OiaBhBSEiwAh2ZSP4lOaJf4kMYk_TnG1dvw92MaCdBP5O9yM75MNM87QBa4H2iPm4pDbBoCfNsQAvD_BwE */
+// var cpm = {};
+// (function(h,u,b){
+// var d=h.getElementsByTagName("script")[0],e=h.createElement("script");
+// e.async=true;e.src='https://cookiehub.net/c2/xxxxxxxx.js';
+// e.onload=function(){u.cookiehub.load(b);}
+// d.parentNode.insertBefore(e,d);
+// })(document,window,cpm);
