54c4920911724dca477e25989bcd420f4eb9b182
Azpers <Azpers@github.com>
2022-10-28 20:30:17 -0300

    js refactored so its better to read

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,31 @@ const main = document.querySelector('main');
 
 /* FETCH FUNCTIONS */
     fetch('https://randomuser.me/api/?inc=name,location,email,dob,cell,picture&results=12&noinfo')
-    .then(response => response.json())
-    .then(data => generateData(data))
-    // .then(data => modalStepsIn(data))
+        .then(response => response.json())
+        .then(data => requestingAPIinfo(data.results))
+        .catch(error => console.log(`Seems an error is happening, ${error}`))
 
+
+    function requestingAPIinfo(data) {
+        generateData(data);
+    }
 
 
 /* HELPER FUNCTIONS */
 
+
+/** Not sure about this one below */
+
+// function checkStatus(response) {
+//     if(response.ok) {
+//       return Promise.resolve(response);
+//     } else {
+//       return Promise.reject(new Error(response.statusText));
+//     }
+//   }
+
 function generateData(data) {
-    data.results.map(result => {
+        data.map(result => {
         const employeeDiv = document.createElement('Div'); 
         employeeDiv.classList.add('card');       
             employeeDiv.innerHTML = `
@@ -22,30 +37,12 @@ function generateData(data) {
                 <p class="ppl-details-fs">${result.email}</p>
                 <p class="ppl-details-fs">${result.location.city}</p>
                 </div>`;
-                //NEW MODAL ATTEMPT V2
-                employeeDiv.addEventListener('click', () => {
-                    const modalWrapper = document.createElement('Div');
-                    modalWrapper.classList.add('modal');
-                    modalWrapper.innerHTML = `
-                    <div class="modal-content">
-                        <span class="close-button"></span>
-                        <img src="${result.picture.medium}" class="img-style">
-                        <p class="name-fs">${result.name.first} ${result.name.last}</p>
-                        <p class="ppl-details-fs">${result.email}</p>
-                        <p class="ppl-details-fs">${result.location.city}</p>
-                    </div>
-                    `;
-                    main.appendChild(modalWrapper);
-                }); //console.log(result.cell)
-        main.appendChild(employeeDiv);
-    });
-    // const allCards = main.getElementsByClassName('card');
-    return data
+            main.appendChild(employeeDiv);
+            employeeDiv.addEventListener('click', () => modalStepsIn(result))
+        })
 }
 
-/* MODAL ATTEMPT V1*/
-
-function modalStepsIn(data) {
+function modalStepsIn(result) {
     const modalWrapper = document.createElement('Div');
     modalWrapper.classList.add('modal');
     modalWrapper.innerHTML = `
