329945375eba83cac842ed9c009868650a910556
Azpers <Azpers@github.com>
2022-10-30 22:55:50 -0300

    right now touching base just in case, it kind of those what I want but I got event listener invoking function its in

--- src/js/app.js
+++ src/js/app.js
@@ -32,7 +32,7 @@ const randomPplArrays = [];
 //kake.map(array => `${array.name.first} ${array.name.last}`);
 //.reduce((acc, curr) => [...acc, ...curr], []);
 function generateData(data) {
-    data.map(result => {
+    data.map((result, index) => {
         const employeeDiv = document.createElement('Div'); 
         employeeDiv.classList.add('card');       
             employeeDiv.innerHTML = `
@@ -43,47 +43,51 @@ function generateData(data) {
                 <p class="ppl-details-fs">${result.location.city}</p>
                 </div>`;
             main.appendChild(employeeDiv);
-            employeeDiv.addEventListener('click', () => modalStepsIn(result))           
+            employeeDiv.addEventListener('click', () => modalStepsIn(data, index))         
         })
 }
-
-function modalStepsIn(result) {
+function modalStepsIn(data, index) {   
     const modalWrapper = document.createElement('Div');
-        modalWrapper.classList.add('modal');
+    modalWrapper.classList.add('modal');
     setTimeout( () => {
         modalWrapper.classList.add('show-modal');
     }, 5)
     modalWrapper.innerHTML = `
     <div class="modal-content">
         <span class="close-button">x</span>
-        <img src="${result.picture.large}" class="img-style-modal">
-        <p class="name-fs-modal">${result.name.first} ${result.name.last}</p>
-        <p class="ppl-details-fs-modal">${result.email}</p>
-        <p class="ppl-details-fs-modal">${result.location.city}</p>
+        <img src="${data[index].picture.large}" class="img-style-modal">
+        <p class="name-fs-modal">${data[index].name.first} ${data[index].name.last}</p>
+        <p class="ppl-details-fs-modal">${data[index].email}</p>
+        <p class="ppl-details-fs-modal">${data[index].location.city}</p>
         <span class="dist-bar"></span>
-        <p class="ppl-details-fs-modal">${result.cell}</p>
-        <p class="ppl-details-fs-modal">${result.location.street.number} ${result.location.street.name}, ${result.location.state} ${result.location.postcode}</p>
-        <p class="ppl-details-fs-modal">Birthday: ${result.dob.date.slice(0,10)}</p>
+        <p class="ppl-details-fs-modal">${data[index].cell}</p>
+        <p class="ppl-details-fs-modal">${data[index].location.street.number} ${data[index].location.street.name}, ${data[index].location.state} ${data[index].location.postcode}</p>
+        <p class="ppl-details-fs-modal">Birthday: ${data[index].dob.date.slice(0,10)}</p>
     </div>
     `;
-    main.appendChild(modalWrapper);    
+main.appendChild(modalWrapper);
+
     modalWrapper.addEventListener('click', () => {
         modalWrapper.remove();
     })
-
-    /*keyboard listener for modal swapper */
-    // window.addEventListener('keydown', (event) => {
-    //     const keyName = event.key;
-    //     const checkVisib = getComputedStyle(modalWrapper).getPropertyValue('visibility');
-    //     if (keyName === 'ArrowRight' | keyName === 'ArrowUp' && checkVisib === 'visible' )
-    //     console.log('forwd')//up n right
-    //     else if(keyName === 'ArrowLeft' | keyName === 'ArrowDown' && checkVisib === 'visible' ) {
-    //         console.log('back')//left and down
-    //     }
-        
-    // })
-    
+window.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    const checkVisib = getComputedStyle(modalWrapper).getPropertyValue('visibility');
+    if (keyName === 'ArrowRight' | keyName === 'ArrowUp' && checkVisib === 'visible' ){
+        modalStepsIn(data, index + 1)
+        console.log('1');//console.log('forwd')//up n right
+    } else if(keyName === 'ArrowLeft' | keyName === 'ArrowDown' && checkVisib === 'visible' ) {
+        console.log('back')//left and down
+    }
+})
 }
+
+
+
+    /*keyboard listener for modal swapper */ 
+
+
+
 
 /* FILTER */
 function searchBar(data) {
